ceac18893d6bb257a7c70017d380696df9f11eda
matcalvani <matstraty@gmail.com>
2022-10-16 23:29:45 -0700

    responsive

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = React.forwardRef((props, ref) => {
     <section id="about">
       <div className="heading-wrapper" ref={ref1}>
         <p className="small-title">Hi, I'm</p>
-        <h1 className="main-title">Matteo Calvani.</h1>
+        <h1 className="main-title">Matteo Calvani</h1>
         <div className="type-writer-wrapper">
           <span style={{ color: '#f08000', fontWeight: 'lighter' }}>Web</span>
           <Typewriter
