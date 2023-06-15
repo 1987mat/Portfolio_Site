a05f4f111a1c8f4d9402dd4eea58071716ea0fe1
matcalvani <matstraty@gmail.com>
2023-06-15 12:17:06 +0200

    update

--- src/data.js
+++ src/data.js
@@ -13,24 +13,10 @@ import weatherApp from './images/weather_app.png';
 export const projects = [
   {
     title: 'Official Artist Website',
-    description: 'Sydney Raneé Wordpress site',
+    description: 'Wordpress custom theme',
     image: sydneyRanee,
     link: 'https://www.sydneyranee.com',
     github: 'https://github.com/1987mat/Sydney_Ranee_Official',
-  },
-  {
-    title: 'React ToDoList App',
-    description: 'CRUD App built with React JS',
-    image: toDoList,
-    link: 'https://1987mat.github.io/ToDoList_ReactJS',
-    github: 'https://github.com/1987mat/ToDoList_ReactJS',
-  },
-  {
-    title: 'eCommerce Fashion Site',
-    description: 'WooCommerce online store ',
-    image: glamourIcon,
-    link: 'https://glamouricon.org/',
-    github: 'https://github.com/1987mat/Syd_Fashion_Store',
   },
   {
     title: 'React Resume Builder',
@@ -40,11 +26,39 @@ export const projects = [
     github: 'https://github.com/1987mat/CV_Application',
   },
   {
+    title: 'React To-do List App',
+    description: 'CRUD App built with React JS',
+    image: toDoList,
+    link: 'https://1987mat.github.io/ToDoList_ReactJS',
+    github: 'https://github.com/1987mat/ToDoList_ReactJS',
+  },
+  {
+    title: 'eCommerce Fashion Site',
+    description: 'Custom WooCommerce theme',
+    image: glamourIcon,
+    link: 'https://glamouricon.org/',
+    github: 'https://github.com/1987mat/Syd_Fashion_Store',
+  },
+  {
+    title: 'Github Finder API App',
+    description: 'Search and display github users profile',
+    image: github,
+    link: 'https://1987mat.github.io/Github_Finder_App',
+    github: 'https://github.com/1987mat/Github_Finder_App',
+  },
+  {
     title: 'WooCommerce Site',
     description: 'Custom WooCommerce theme',
     image: sydneyStore,
     link: 'https://sydneyraneeshop.com',
     github: 'https://github.com/1987mat/Sydney-Ranee-Store',
+  },
+  {
+    title: 'Library App',
+    description: 'CRUD App built with Vanilla JS',
+    image: library,
+    link: 'https://1987mat.github.io/LibraryApp',
+    github: 'https://github.com/1987mat/LibraryApp',
   },
   {
     title: 'Gym Website ',
@@ -54,40 +68,24 @@ export const projects = [
     github: 'https://github.com/1987mat/FitWellGym',
   },
   {
+    title: 'Weather App',
+    description: 'Weather forescast API app',
+    image: weatherApp,
+    link: 'https://1987mat.github.io/Weather_App',
+    github: 'https://github.com/1987mat/Weather_App',
+  },
+  {
     title: 'Coffee Shop Site',
     description: 'Custom Wordpress theme',
     image: cafeDolce,
     link: 'https://www.cafedolce.org/',
     github: 'https://github.com/1987mat/Cafe_Dolce',
   },
-
   {
     title: 'Restaurant Brochure Site',
     description: 'Wordpress site built with Elementor ',
     image: origanoTrattoria,
     link: 'https://origanotrattoria.org/',
     github: 'https://github.com/1987mat/restaurant_elementor_child_theme',
-  },
-  {
-    title: 'Github Finder App',
-    description: 'Search and display github users profile',
-    image: github,
-    link: 'https://1987mat.github.io/Github_Finder_App',
-    github: 'https://github.com/1987mat/Github_Finder_App',
-  },
-
-  {
-    title: 'Library App',
-    description: 'CRUD App built with Vanilla JS',
-    image: library,
-    link: 'https://1987mat.github.io/LibraryApp',
-    github: 'https://github.com/1987mat/LibraryApp',
-  },
-  {
-    title: 'Weather App',
-    description: 'Weather forescast API app',
-    image: weatherApp,
-    link: 'https://1987mat.github.io/Weather_App',
-    github: 'https://github.com/1987mat/Weather_App',
   },
 ];
